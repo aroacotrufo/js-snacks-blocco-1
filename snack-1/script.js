f9fd4ea3feaa26ac00356938815ac244b96af162
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-05 11:02:04 +0100

    correzione

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,6 +3,9 @@ const secondNumber = Number(prompt("Inserisci un'altro numero"))
 
 if(firstNumbers<secondNumber){
     console.log(secondNumber)
-}else{
+}else if(secondNumber<firstNumbers){
     console.log(firstNumbers)
+}else{
+    const message = 'I numeri sono uguali'
+    console.log(message)
 }
