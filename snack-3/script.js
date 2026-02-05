99320f2a88604d283ee82f058ee1dba8cfc535a6
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-05 12:41:47 +0100

    correzione

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,6 +1,6 @@
 let somma = 0
-for (let i=0; i<=10;i++){
-    const number = Number(prompt(`inserisci un numero ${i+1}`))
-     number+=somma         
+for (let i=0; i<10;i++){
+    let number = Number(prompt(`inserisci un numero ${i+1}`))
+    somma += number             //somma = somma + number
     console.log(somma)
 }
