8bcbc3303f4223e1fb7bd276e57b6ebda078a296
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-05 10:33:17 +0100

    esercizio completato

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const firstNumbers = Number(prompt("Inserisci un numero"))
+const secondNumber = Number(prompt("Inserisci un'altro numero"))
+
+if(firstNumbers<secondNumber){
+    console.log(secondNumber)
+}else{
+    console.log(firstNumbers)
+}
